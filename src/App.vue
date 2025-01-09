d3627fb4e78cf580e5a06d61ecff7138ea38d5e2
zhoucheng <zhoucheng133@outlook.com>
2025-01-09 16:21:36 +0800

    添加是否需要登录判定

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <RouterView/>
 </template>
 
-<script lang="ts">
-import { RouterView } from 'vue-router';
+<script lang="ts" setup>
+import axios from 'axios';
+import { RouterView, useRouter } from 'vue-router';
+import hostname from './hooks/hostname';
+const router=useRouter();
+const init=async ()=>{
+  const {data: auth}=await axios.get(`${hostname}/api/auth`);
+  if(auth.useAuth){
+    router.replace("/login");
+  }
+}
+
+init();
 </script>
