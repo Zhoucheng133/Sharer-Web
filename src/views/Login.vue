10281b1bc884633f62bd8a2f6d0e07b311cc23c1
zhoucheng <zhoucheng133@outlook.com>
2025-01-09 16:22:49 +0800

    添加阻止进入login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,4 +3,17 @@
 </template>
 
 <script lang="ts" setup>
+import axios from 'axios';
+import hostname from '../hooks/hostname';
+import { useRouter } from 'vue-router';
+
+const router=useRouter();
+const init=async ()=>{
+  const {data: auth}=await axios.get(`${hostname}/api/auth`);
+  if(!auth.useAuth){
+    router.replace("/");
+  }
+}
+
+init();
 </script>
