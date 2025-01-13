d9c18d38b2503a1211c4150f9f6f9b874cb76201
zhoucheng <zhoucheng133@outlook.com>
2025-01-13 19:37:44 +0800

    改用直链预览

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -3,12 +3,12 @@
     <div class="preview_title_bar">
       <div class="preview_title">{{ preview().previewFile }}</div>
       <div class="preview_tools">
-        <Button label="下载" size="small" style="margin-right: 10px;" @click="preview().download" />
+        <Button label="下载" size="small" style="margin-right: 10px;"/>
         <Button icon="pi pi-times" variant="text" rounded aria-label="Filter" size="small" @click="preview().closePreview" />
       </div>
     </div>
     <div class="preview_content">
-      <div class="preview_media" v-if="previewType==Types.Video">
+      <div class="preview_media" v-if="preview().previewType==Types.Video">
         <video ref="playerContainer" controls playsinline>
           <source ref="mediaSource" :src="preview().previewUrl" />
         </video>
@@ -22,15 +22,12 @@
 import "../styles/preview.css";
 import Plyr from "plyr";
 import 'plyr/dist/plyr.css';
-import { nextTick, ref, watch } from "vue";
+import { onMounted, ref } from "vue";
 import preview from "../hooks/preview";
 import { Types } from "../hooks/static";
-import { storeToRefs } from "pinia";
 import { Button } from "primevue";
 const playerContainer=ref(null);
 const mediaSource=ref<any>(null);
-
-const {previewType}=storeToRefs(preview());
 
 window.addEventListener("keydown", (e)=>{
   if(e.key=='Escape'){
@@ -38,46 +35,43 @@ window.addEventListener("keydown", (e)=>{
   }
 })
 
-watch(previewType, async (newVal)=>{
-  if(newVal==Types.Video){
-    await nextTick();
-    if(playerContainer.value){
-      new Plyr(playerContainer.value, {
-        i18n: {
-          restart: '重新开始',
-          rewind: '倒退 {seektime} 秒',
-          play: '播放',
-          pause: '暂停',
-          fastForward: '快进 {seektime} 秒',
-          mute: '静音',
-          unmute: '取消静音',
-          volume: '音量',
-          settings: '设置',
-          captions: '字幕',
-          disabled: '已禁用',
-          enabled: '已启用',
-          loop: '循环播放',
-          speed: '播放速度',
-          normal: '正常',
-          quality: '质量',
-          download: '下载',
-          fullscreen: '全屏',
-          exitFullscreen: '退出全屏',
-        },
-        controls: [
-          'play',
-          'progress',
-          'current-time',
-          'mute',
-          'volume',
-          'settings', // 启用设置选项
-          'fullscreen',
-        ],
-      });
-      return {
-        playerContainer,
-      };
-    }
+onMounted(()=>{
+  if(preview().previewType==Types.Video && playerContainer.value){
+    new Plyr(playerContainer.value, {
+      i18n: {
+        restart: '重新开始',
+        rewind: '倒退 {seektime} 秒',
+        play: '播放',
+        pause: '暂停',
+        fastForward: '快进 {seektime} 秒',
+        mute: '静音',
+        unmute: '取消静音',
+        volume: '音量',
+        settings: '设置',
+        captions: '字幕',
+        disabled: '已禁用',
+        enabled: '已启用',
+        loop: '循环播放',
+        speed: '播放速度',
+        normal: '正常',
+        quality: '质量',
+        download: '下载',
+        fullscreen: '全屏',
+        exitFullscreen: '退出全屏',
+      },
+      controls: [
+        'play',
+        'progress',
+        'current-time',
+        'mute',
+        'volume',
+        'settings',
+        'fullscreen',
+      ],
+    });
+    return {
+      playerContainer,
+    };
   }
 })
 </script>
--- src/hooks/preview.ts
+++ src/hooks/preview.ts
@@ -1,32 +1,22 @@
 import { defineStore } from "pinia";
 import { ref, watch } from "vue";
 import { getFileType, Types } from "./static";
-import axios from "axios";
 import hostname from "./hostname";
 import store from "./store";
 
 export default defineStore("preview", ()=>{
   let previewFile=ref<string>("");
   let previewType = ref<Types | undefined>(undefined);
-  let previewUrl=ref("");
   let exit=ref<boolean>(false);
+  let previewUrl=ref("");
 
   watch(previewFile, async (newVal)=>{
     if(newVal.length==0){
       return;
-    }else if(previewUrl.value){
-      URL.revokeObjectURL(previewUrl.value);
     }
-    const response=await axios.get(`${hostname}/api/raw?path=${store().pathResolve}${newVal}`, {
-      headers: {
-        token: store().token
-      },
-      responseType: "blob"
-    })
-    const mimeType = response.headers['content-type'] || 'application/octet-stream'; 
-    const videoBlob = new Blob([response.data], { type: mimeType });
-    previewUrl.value=URL.createObjectURL(videoBlob);
-    previewType.value=getFileType(newVal);
+    previewType.value=getFileType(previewFile.value);
+    // console.log(previewType.value);
+    previewUrl.value=`${hostname}/api/raw?path=${store().pathResolve}${previewFile.value}&token=${store().token}`;
   })
 
   const closePreview=()=>{
@@ -35,28 +25,9 @@ export default defineStore("preview", ()=>{
       previewFile.value="";
       exit.value=false;
       previewType.value=undefined;
+      previewUrl.value="";
     }, 200);
   }
 
-  const download=async ()=>{
-    const response = await axios.get(`${hostname}/api/download?path=${encodeURIComponent(store().pathResolve) + encodeURIComponent(previewFile.value)}`,
-      {
-        headers: {
-          token: store().token,
-        },
-        responseType: 'blob',
-      }
-    );
-    const headers = response.headers;
-    
-    const blobUrl = URL.createObjectURL(response.data);
-    const a = document.createElement('a');
-    a.href = blobUrl;
-    a.download = decodeURIComponent(headers['file-name']); 
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
-
-  return {previewFile, previewType, previewUrl, closePreview, exit, download};
+  return {previewFile, previewType, previewUrl, closePreview, exit};
 })
