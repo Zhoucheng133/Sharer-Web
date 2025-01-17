73ac7f9e1694e4e79a6d7043cae6f22a11543dac
zhoucheng <zhoucheng133@outlook.com>
2025-01-17 19:42:56 +0800

    添加移动端抽屉的一些功能

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,10 +1,28 @@
 <template>
   <Drawer v-model:visible="drawer().showDrawer" header="文件操作" position="bottom" style="height: auto">
-    
+    <div class="drawer_item" @click="drawer().download">
+      <div class="drawer_item_icon">
+        <i class="pi pi-download"></i>
+      </div>
+      <div class="drawer_item_label">下载</div>
+    </div>
+    <div class="drawer_item" @click="drawer().rename">
+      <div class="drawer_item_icon">
+        <i class="pi pi-pen-to-square"></i>
+      </div>
+      <div class="drawer_item_label">重命名</div>
+    </div>
+    <div class="drawer_item" style="color: red;" @click="drawer().del">
+      <div class="drawer_item_icon">
+        <i class="pi pi-trash"></i>
+      </div>
+      <div class="drawer_item_label">删除</div>
+    </div>
   </Drawer>
 </template>
 
 <script lang="ts" setup>
 import Drawer from 'primevue/drawer';
 import drawer from '../hooks/drawer';
+import "../styles/drawer.css";
 </script>
--- src/hooks/drawer.ts
+++ src/hooks/drawer.ts
@@ -1,15 +1,42 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import type { FileItem } from "./store";
+import { delHandler, downloadHandler, renameHandler } from "./handler";
 
 export default defineStore("drawer", ()=>{
   let showDrawer=ref(false);
-  let selectedItem=ref<FileItem>();
+  let selectedItem=ref<FileItem>({
+    name: "",
+    size: 0,
+    isDir: false,
+    selected: false
+  });
+
+  function download(){
+    downloadHandler(selectedItem.value);
+    showDrawer.value=false;
+  }
+
+  function del(){
+    
+  }
+
+  function rename(){
+    showDrawer.value=false;
+    renameHandler(selectedItem.value)
+  }
 
   function toggleDrawer(item: FileItem){
     selectedItem.value=item;
     showDrawer.value=true;
   }
 
-  return {showDrawer, selectedItem, toggleDrawer}
+  return {
+    showDrawer,
+    selectedItem,
+    toggleDrawer,
+    download,
+    del,
+    rename
+  }
 })
